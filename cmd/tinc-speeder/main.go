aa60125e3752b2c2e804e67257fb388b1fcced8f
yangjun <290968698@qq.com>
2025-12-12 14:52:07 +0800

    init go proj

---
+++ cmd/tinc-speeder/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	tinc_speeder "github.com/yangjuncode/tinc-speeder"
+)
+
+func main() {
+	fmt.Println("tinc-speeder version:", tinc_speeder.Version)
+}
---
+++ version.go
@@ -0,0 +1,6 @@
+package tinc_speeder
+
+import _ "embed"
+
+//go:embed version.txt
+var Version string
